5a85789562591ed6367909f1ed6c4927da54d04b
wx2020 <35266946+wx2020@users.noreply.github.com>
2026-09-14 03:32:00 +0000

    fix: EpisodeProvider 补 MetaSource 命名空间引用
    修复 CS0103：写入集级标记用到 MetaSource，补 using Jellyfin.Plugin.MetaShark.Model。

--- Jellyfin.Plugin.MetaShark/Providers/EpisodeProvider.cs
+++ Jellyfin.Plugin.MetaShark/Providers/EpisodeProvider.cs
@@ -1,5 +1,6 @@
 ﻿using Jellyfin.Plugin.MetaShark.Api;
 using Jellyfin.Plugin.MetaShark.Core;
+using Jellyfin.Plugin.MetaShark.Model;
 using Jellyfin.Plugin.MetaShark.StrmProbe;
 using MediaBrowser.Controller.Entities.TV;
 using MediaBrowser.Controller.Library;
